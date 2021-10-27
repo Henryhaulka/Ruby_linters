f25d522b8727c7bdfe730f0b0805d8f2a2ab31db
Henry Onu <onuhenry24@gmail.com>
2021-10-27 13:42:55 +0100

    Fix rubocop error

--- lib/checker.rb
+++ lib/checker.rb
@@ -1,7 +1,8 @@
 require_relative 'file'
 class Checker
-    attr_reader :check
-    def initialize(file)
-        @check = FileRead.new(file)
-    end
+  attr_reader :check
+
+  def initialize(file)
+    @check = FileRead.new(file)
+  end
 end
--- lib/file.rb
+++ lib/file.rb
@@ -1,16 +1,17 @@
 require 'colorize'
 
 class FileRead
-    attr_reader :file, :file_line_counter, :error_message, :file_read
-    def initialize(file)
-        @file = file
-        @error_message = ''
-        begin
-           @file_read = File.readlines(file)
-           @file_line_counter = file_read.length 
-        rescue StandardError => error
-           @error_message = "Ensure typed the right path or extension\n".colorize(:yellow) +
-           error.to_s.colorize(:red)
-        end
+  attr_reader :file, :file_line_counter, :error_message, :file_read
+
+  def initialize(file)
+    @file = file
+    @error_message = ''
+    begin
+      @file_read = File.readlines(file)
+      @file_line_counter = file_read.length
+    rescue StandardError => e
+      @error_message = "Ensure typed the right path or extension\n".colorize(:yellow) +
+                       e.to_s.colorize(:red)
     end
+  end
 end
