845a8660562f4366d3b1be9ffa4c35d7d2ee6e8c
Henry Onu <onuhenry24@gmail.com>
2021-10-28 16:18:03 +0100

    Add error check logics and test.rb

--- bin/main.rb
+++ bin/main.rb
@@ -2,3 +2,15 @@ require_relative '../lib/checker'
 # 'Checker' class takes the preloaded file,
 test = Checker.new(ARGV[0])
 puts test.check.error_message if StandardError
+
+test.trailing_space
+test.line_error
+if test.lint_error.empty?
+  puts "File inspected #{ARGV.size}: #{(ARGV[0]).colorize(:blue)} :#{'No offense'.colorize(:green)} detected"
+else
+  # puts "#{test.check.file.colorize(:light_blue)}: #{test.lint_error.join.colorize(:red)}"
+  test.lint_error.each do |error|
+    puts "#{test.check.file.colorize(:light_blue)}: #{error.colorize(:red)}"
+  end
+end
+p test.check.file_line_counter
--- lib/test.rb
+++ lib/test.rb
@@ -1,1 +1,17 @@
-as
+class ClassName
+
+   to
+end
+
+
+#always remember that after a method or a block of code, there should be a line break
+#and the new line is not counted as a line by the computer
+
+def method_name
+
+    def
+    end
+
+    def
+    end
+end
